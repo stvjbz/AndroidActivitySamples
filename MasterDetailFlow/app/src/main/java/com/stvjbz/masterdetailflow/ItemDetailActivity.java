14c8451a75c9400478d8abf3746b4e7c4a7d4f27
stvjbz <stvjbz@outlook.com>
2016-05-29 18:06:04 +0900

    [add] masterdetailflow/Empty files.

---
+++ MasterDetailFlow/app/src/main/java/com/stvjbz/masterdetailflow/ItemDetailActivity.java
@@ -0,0 +1,12 @@
+package com.stvjbz.masterdetailflow;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ItemDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
--- MasterDetailFlow/app/src/main/java/com/stvjbz/masterdetailflow/ItemListActivity.java
+++ MasterDetailFlow/app/src/main/java/com/stvjbz/masterdetailflow/ItemListActivity.java
@@ -3,11 +3,10 @@ package com.stvjbz.masterdetailflow;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ItemListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
     }
 }
